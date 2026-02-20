4b132b15c827ab7384f27d538a4563124a262d6d
Megaman <pacosclub@gmail.com>
2026-02-19 20:57:00 -0600

    fix(Fonts & Config): Se termina con la configuración de los archivos eslint.config.js, vite.config.js y package.json para cambiar comillas dobles por simples en los archivos del proyecto. También se agregó el formato .woff a la configuración de las fuentes.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "../Main/Main.jsx";
-import SavedNews from "../SavedNews/SavedNews.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from '../Main/Main.jsx';
+import SavedNews from '../SavedNews/SavedNews.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Header />
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/saved-news" element={<SavedNews />} />
+          <Route path='/' element={<Main />} />
+          <Route path='/saved-news' element={<SavedNews />} />
         </Routes>
         <Footer />
       </div>
--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,11 +1,11 @@
-import "./Preloader.css";
+import './Preloader.css';
 
 export default function Preloader() {
   return (
-    <div className="preloader">
-      <div className="preloader__container">
-        <span className="preloader__round"></span>
-        <p className="preloader__text">Buscando noticias...</p>
+    <div className='preloader'>
+      <div className='preloader__container'>
+        <span className='preloader__round'></span>
+        <p className='preloader__text'>Buscando noticias...</p>
       </div>
     </div>
   );
