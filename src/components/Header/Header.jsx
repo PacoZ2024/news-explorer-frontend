93b8984a55a37fc9f4554966d6be171adb47275e
Megaman <pacosclub@gmail.com>
2026-02-22 05:53:56 -0600

    feat(Header): Se agrega el modo obscuro a los elementos del encabezado así como la opción de Artículos guardados cuando el usuario ingresa a su cuenta.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,14 @@ export default function Header() {
           News Explorer
         </Link>
         <div className='header__link-distributions'>
-          <div className='header__content-link'>
+          <div className='header__link-container header__link-container-active'>
             <Link className='header__link' to='/'>
               Inicio
+            </Link>
+          </div>
+          <div className='header__link-container header__link-container-disabled'>
+            <Link className='header__link' to='/saved-news'>
+              Artículos guardados
             </Link>
           </div>
           <button className='header__button'>Iniciar sesión</button>
