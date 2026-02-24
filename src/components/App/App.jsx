1f6782c007b8512a50cfa5af648453f19980dffb
Megaman <pacosclub@gmail.com>
2026-02-24 00:46:43 -0600

    feat(About): Se agrega el componente Acerca de Mí. También se generalizan algunos estilos en cuanto al centrado de componentes y se corrigen otros referentes al componente App. Por otro lado se cambia del componente App a main el BrowserRouter. Y se comenta un estilo de preloader que evitaba el scroll de la app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from '../Main/Main.jsx';
 import Header from '../Header/Header.jsx';
 import SavedNews from '../SavedNews/SavedNews.jsx';
@@ -6,15 +6,13 @@ import Footer from '../Footer/Footer.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/saved-news' element={<SavedNews />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className='app'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/saved-news' element={<SavedNews />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import SearchForm from '../SearchForm/SearchForm.jsx';
+import About from '../About/About.jsx';
 
 export default function Main() {
   return (
-    <main>
+    <main className='main'>
       <SearchForm />
+      <About />
     </main>
   );
 }
