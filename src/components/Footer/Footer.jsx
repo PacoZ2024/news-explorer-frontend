61b5de11ef69ff0a843862810ba9009e91c3f52e
Megaman <pacosclub@gmail.com>
2026-02-24 18:44:33 -0600

    feat(Footer): Se agrega el componente Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,49 @@
+import { Link } from 'react-router-dom';
+import iconFacebook from '../../assets/images/icon_fb.svg';
+import iconGithub from '../../assets/images/icon_github.svg';
+
 export default function Footer() {
-  return <p>Este es el footer</p>;
+  return (
+    <footer className='footer'>
+      <div className='footer__content'>
+        <p className='footer__copyright'>
+          &#169; 2026 Supersite, Powered by News API
+        </p>
+        <div className='footer__link-content'>
+          <div className='footer__link-container'>
+            <Link className='footer__link' to='/'>
+              Inicio
+            </Link>
+            <Link className='footer__link' to='/'>
+              Practicum
+            </Link>
+          </div>
+          <div className='footer__icon-container'>
+            <a
+              className='footer__link-icon'
+              href='https://github.com'
+              target='_blank'
+            >
+              <img
+                className='footer__icon'
+                alt='icono GitHub'
+                src={iconGithub}
+              />
+            </a>
+            <a
+              className='footer__link-icon'
+              href='https://www.facebook.com'
+              target='_blank'
+            >
+              <img
+                className='footer__icon'
+                alt='icono Facebook'
+                src={iconFacebook}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
