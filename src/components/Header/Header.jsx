b6f38b1aa66033d5d421d962424c96aa2f4cf909
Megaman <pacosclub@gmail.com>
2026-02-25 18:46:35 -0600

    fix(Header): Se incorporan algunos estilos al componente Header correspondiente a la sección Artículos guardados

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,23 +2,25 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='header'>
+    <header className='header header__dark'>
       <div className='header__content'>
         <Link className='header__logo' to='/'>
           News Explorer
         </Link>
         <div className='header__link-distributions'>
-          <div className='header__link-container header__link-container-active'>
+          <div className='header__link-container'>
             <Link className='header__link' to='/'>
               Inicio
             </Link>
           </div>
-          <div className='header__link-container header__link-container-disabled'>
+          <div className='header__link-container header__link-container-active-dark'>
             <Link className='header__link' to='/saved-news'>
               Artículos guardados
             </Link>
           </div>
-          <button className='header__button'>Iniciar sesión</button>
+          <button className='header__button header__button-dark'>
+            Iniciar sesión
+          </button>
         </div>
       </div>
     </header>
