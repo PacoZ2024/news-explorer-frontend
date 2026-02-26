b297ec8dda3403949c7570fa0ffd89f83af1dd8b
Megaman <pacosclub@gmail.com>
2026-02-26 17:55:16 -0600

    feat(Preloader): Se ajusta el componente Preloader, así como sus estilos, al proyecto.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import SearchForm from '../SearchForm/SearchForm.jsx';
 import About from '../About/About.jsx';
+import Preloader from '../Preloader/Preloader.jsx';
 
 export default function Main() {
   return (
     <main className='main'>
       <SearchForm />
+      <Preloader />
       <About />
     </main>
   );
--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,9 +1,7 @@
-import './Preloader.css';
-
 export default function Preloader() {
   return (
     <div className='preloader'>
-      <div className='preloader__container'>
+      <div className='preloader__content'>
         <span className='preloader__round'></span>
         <p className='preloader__text'>Buscando noticias...</p>
       </div>
