665dacf84854cbe818dff4c85a095dd36741c8d3
Megaman <pacosclub@gmail.com>
2026-02-28 00:17:35 -0600

    feat(NewsCardList & NewsCard): Se organizan adecuadamente las tarjetas de noticias, conforme a diseño, en el componente NewsCardList. También se ajustan los márgenes de las tarjetas en NewsCard y se suben algunas tarjetas de prueba.

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,26 +1,19 @@
-import natureNotice from '../../assets/images/nature-notice.jpg';
-
-export default function NewsCard() {
+export default function NewsCard(props) {
+  const { image, date, title, content, source } = props.card;
   return (
     <div className='news-card'>
       <div className='news-card__image-container'>
         <img
           className='news-card__image'
-          alt='Noticia de naturaleza'
-          src={natureNotice}
+          alt='Imagen de la noticia'
+          src={image}
         />
       </div>
       <div className='news-card__info'>
-        <p className='news-card__date'>4 de noviembre de 2020</p>
-        <h3 className='news-card__title'>
-          Todo el mundo necesita un lugar de reflexión en la naturaleza.
-        </h3>
-        <p className='news-card__paragraph'>
-          Desde que leí el influyente libro de Richard Louv, "El último niño en
-          el bosque", la idea de tener un "lugar de reflexión" especial para mi
-          se me ha quedado grabada. Este consejo, que...
-        </p>
-        <p className='news-card__source'>treehugger</p>
+        <p className='news-card__date'>{date}</p>
+        <h3 className='news-card__title'>{title}</h3>
+        <p className='news-card__paragraph'>{content}</p>
+        <p className='news-card__source'>{source}</p>
       </div>
     </div>
   );
