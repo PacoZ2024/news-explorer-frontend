59852dbe50fbb125803532cc88f74393d3eae54e
Megaman <pacosclub@gmail.com>
2026-03-08 16:38:40 -0600

    fix(News Card): Se considera el caso en el que se encuentran en la sección -Artículos guardados- y no se deben renderizar los artículos que no estan guardados; además se coloca el parámetro -alt- a distintas etiquetas <img> que carecian de ello.

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -33,62 +33,73 @@ export default function NewsCard({
   }
 
   return (
-    <div className='news-card'>
-      <div className='news-card__image-container'>
-        {location.pathname === '/saved-news' ? (
-          <div className='news-card__keyword'>{keyword}</div>
-        ) : (
-          <></>
-        )}
-        <img
-          className='news-card__image'
-          alt='Imagen de la noticia'
-          src={image}
-        />
-        <div className='news-card__button-container'>
-          {location.pathname === '/saved-news' ? (
-            <button
-              className='news-card__delete-button'
-              onClick={handleSaveClick}
-              onMouseEnter={() => {
-                setMessageTooltip('Eliminar de artículos guardados');
-                isSaved && isLoggedIn && setShowTooltip(true);
-              }}
-              onMouseLeave={() => setShowTooltip(false)}
-            >
-              <img className='news-card__delete-icon' src={iconTrash} />
-            </button>
-          ) : (
-            <button
-              className='news-card__save-button'
-              onClick={handleSaveClick}
-              onMouseEnter={() => {
-                setMessageTooltip('Inicia sesión para guardar artículos');
-                !isLoggedIn && setShowTooltip(true);
-              }}
-              onMouseLeave={() => setShowTooltip(false)}
-            >
-              {isSaved && isLoggedIn ? (
-                <img
-                  className='news-card__save-icon news-card__save-icon_active'
-                  src={iconSavedActive}
-                />
+    <>
+      {location.pathname === '/saved-news' && !isSaved ? (
+        <></>
+      ) : (
+        <div className='news-card'>
+          <div className='news-card__image-container'>
+            {location.pathname === '/saved-news' ? (
+              <div className='news-card__keyword'>{keyword}</div>
+            ) : (
+              <></>
+            )}
+            <img className='news-card__image' alt={title} src={image} />
+            <div className='news-card__button-container'>
+              {location.pathname === '/saved-news' ? (
+                <button
+                  className='news-card__delete-button'
+                  onClick={handleSaveClick}
+                  onMouseEnter={() => {
+                    setMessageTooltip('Eliminar de artículos guardados');
+                    setShowTooltip(true);
+                  }}
+                  onMouseLeave={() => setShowTooltip(false)}
+                >
+                  <img
+                    className='news-card__delete-icon'
+                    src={iconTrash}
+                    alt='Icono para quitar el artículo de guardados'
+                  />
+                </button>
               ) : (
-                <img className='news-card__save-icon' src={iconSaved} />
+                <button
+                  className='news-card__save-button'
+                  onClick={handleSaveClick}
+                  onMouseEnter={() => {
+                    setMessageTooltip('Inicia sesión para guardar artículos');
+                    !isLoggedIn && setShowTooltip(true);
+                  }}
+                  onMouseLeave={() => setShowTooltip(false)}
+                >
+                  {isSaved && isLoggedIn ? (
+                    <img
+                      className='news-card__save-icon news-card__save-icon_active'
+                      src={iconSavedActive}
+                      alt='Icono para indicar que un artículo esta guardado'
+                    />
+                  ) : (
+                    <img
+                      className='news-card__save-icon'
+                      src={iconSaved}
+                      alt='Icono sin rellenar para indicar que un artículo puede ser almacenado'
+                    />
+                  )}
+                </button>
               )}
-            </button>
-          )}
-          {showTooltip && (
-            <div className='news-card__tooltip'>{messageTooltip}</div>
-          )}
+              {showTooltip && (
+                <div className='news-card__tooltip'>{messageTooltip}</div>
+              )}
+            </div>
+          </div>
+          <div className='news-card__info'>
+            <p className='news-card__date'>{date}</p>
+            <h3 className='news-card__title'>{title}</h3>
+            <p className='news-card__paragraph'>{content}</p>
+            <p className='news-card__source'>{source}</p>
+          </div>
         </div>
-      </div>
-      <div className='news-card__info'>
-        <p className='news-card__date'>{date}</p>
-        <h3 className='news-card__title'>{title}</h3>
-        <p className='news-card__paragraph'>{content}</p>
-        <p className='news-card__source'>{source}</p>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
