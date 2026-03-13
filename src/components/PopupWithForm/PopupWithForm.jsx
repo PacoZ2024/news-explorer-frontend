a1c743355bbe5c5403d011ae93e7ff3ef9704988
Megaman <pacosclub@gmail.com>
2026-03-12 20:31:08 -0600

    feat(PopupWithForm): Se agrega la funcionalidad de cerrar el popup pulsando la tecla Esc o dando click en el Overlay. También se agrega el estilo para eliminar el borde de los botones al hacer click en ellos

--- src/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/PopupWithForm/PopupWithForm.jsx
@@ -1,8 +1,33 @@
+import { useEffect } from 'react';
 import closeButton from '../../assets/images/icon-close-popup.svg';
 
 export default function PopupWithForm({ onClosePopup, children }) {
+  useEffect(() => {
+    function handleEscClose(evt) {
+      if (evt.key === 'Escape') {
+        onClosePopup();
+      }
+    }
+
+    if (children) {
+      document.addEventListener('keydown', handleEscClose);
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscClose);
+      document.body.style.overflow = 'unset';
+    };
+  }, [children, onClosePopup]);
+
+  function handleOverlayClick(evt) {
+    if (evt.target === evt.currentTarget) {
+      onClosePopup();
+    }
+  }
+
   return (
-    <div className='popup-with-form'>
+    <div className='popup-with-form' onClick={handleOverlayClick}>
       <div className='popup-with-form__content'>
         <button
           className='popup-with-form__close-button'
